e9b3b4270772159e4337ed2996f5e1507511fc6b
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-10-03 17:29:37 -0400

    project init

---
+++ NYCSchools/NYCSchools/NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsApp.swift
+//  NYCSchools
+//
+//  Created by Weiqi Zhuang on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
